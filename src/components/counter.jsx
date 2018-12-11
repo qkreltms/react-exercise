2740a6f204d1b48335ef10514f535aafc37c8c08
JungHoon Park <wfr112@naver.com>
2018-12-11 19:29:16 +0900

    Add navbar, App Update counter, counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,53 +1,61 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: this.props.value,
-    imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"]
-  };
+  //컴포넌트 업데이트 후 호출
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps', prevProps)
+    console.log('prevState', prevState)
+    if (prevProps.counter.value !== this.props.counter.value) {
+      // Ajax call and get new data from the server
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('Counter - Unmount')
+    //컴포넌트 없애기전 사용 메모리 초기화해 메모리 누수방지 가능
+  }
 
   styles = {
     fontWeight: "bold",
     fontSize: 10
   };
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  };
-
-  handleIncrement = () => {
-      this.setState({ count: this.state.count + 1 });
-  };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags</p>;
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
   render() {
+    console.log('Counter - Rendered');
+
     return (
       //React.Fragment div가 중복되지 않게 해줌.
       <React.Fragment>
+        <h4>{this.props.counter.id}</h4>
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button onClick={ () => this.handleIncrement()} className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </React.Fragment>
     );
-  }
+  };
+
+  formatCount() {
+    const count = this.props.counter.value;
+    return count === 0 ? "Zero" : count;
+  };
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
-  }
+  };
 }
 
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import Counter from "./counter";
 
-class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
-  render() {
+const Counters = ({ onReset, counters, onDelete, onIncrement }) => {
+    console.log('Counters - Rendered');
+
     return (
-      <div>
-        {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value}/>
-        ))}
-      </div>
-    );
-  }
-}
+    <div>
+      <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+        Reset
+      </button>
+      {counters.map(counter => (
+        <Counter
+          key={counter.id}
+          counter={counter}
+          onDelete={onDelete}
+          onIncrement={onIncrement}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Counters;
